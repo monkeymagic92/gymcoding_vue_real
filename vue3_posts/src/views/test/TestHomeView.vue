6a27bf79880eafba8150c197d906df841b927f36
monkeymagic92 <ddw0099@naver.com>
2024-04-30 14:47:46 +0900

    Test

--- vue3_posts/src/views/test/TestHomeView.vue
+++ vue3_posts/src/views/test/TestHomeView.vue
@@ -5,11 +5,33 @@
     <button type="button" @click="moveChild2">child2</button>
     <hr>
     <RouterView></RouterView>
+
+    <br><hr><hr><br>
+    {{ obj.name }}<br>
+    {{ obj.age }}
+    <br>
+    {{ testData }}
+    {{ testData2 }}
+
+    <TestTestView :obj="obj" @fnTest="fnTest"></TestTestView>
+    
   </div>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
+import TestTestView from "./TestTestView.vue";
+import { ref } from "vue";
+  
+  let obj = ref({});
+  const testData = ref('');
+  const testData2 = ref('');
+
+  const fnTest = (p1, p2) => {
+    testData.value = p1;
+    testData2.value = p2;
+  }
+  
 
   const router = useRouter();
 
@@ -18,13 +40,14 @@ import { useRouter } from "vue-router";
       name: 'TestChild1'
     });
   }
-  
 
   const moveChild2 = () => {
     router.push({
       name: 'TestChild2'
     });
   }
+  
+  
 
 
 </script>
---
+++ vue3_posts/src/views/test/TestTestView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>TestTestView</h1>
+    <br>
+    <input type="text" v-model="obj.name" />
+    <input type="text" v-model="obj.age" />
+    <br>
+    <button type="button" @click="$emit('fnTest', 'Test내용', 'Test2내용')">버튼</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  obj: {
+    type: Number,
+    
+  }
+})
+
+const title = ref('');
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
